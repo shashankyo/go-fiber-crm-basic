f3d7249eace88cfa4a40d68c9165bd452fb5f6de
shashank <shashank944533@gmail.com>
2022-11-22 13:10:32 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/shashank/go-fiber-crm-basic/database"
+	"github.com/shashank/go-fiber-crm-basic/lead"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close()
+}
